0a0874ec38ff3faa8a232053b3b84bef7adba4f1
romeo kubeka <romeokubeka@gmail.com>
2023-07-19 08:32:43 +0000

    Add event listeners for game start, title click, and instructions button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,30 @@
+// game.js
+
+document.getElementById("start-button").addEventListener("click", function() {
+    // Show the game container and hide the menu
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("game-container").style.display = "block";
+});
+
+document.getElementById("game-title").addEventListener("click", function() {
+    document.getElementById("game-container").style.display = "none";
+    document.getElementById("menu").style.display = "block";
+});
+
+document.getElementById("instructions-button").addEventListener("click", function() {
+    const instructions = "How to Play:\n\n"
+        + "1. Pick between rock, paper, or scissors.\n"
+        + "2. The computer will also choose from one of the options.\n"
+        + "3. Determine the winner based on the rules:\n"
+        + "   - Rock beats scissors.\n"
+        + "   - Paper beats rock.\n"
+        + "   - Scissors beat paper.\n"
+        + "4. Keep track of your wins and losses.";
+    
+    alert(instructions);
+});
+
+
 function createComputerChoice() {
 
 }
