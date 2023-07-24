e911454c3503a7282fae33de1b5470f10d010f3e
romeo kubeka <romeokubeka@gmail.com>
2023-07-24 11:50:34 +0000

    Add 10 rounds/game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,72 @@
-// game.js
+// Cache DOM elements
+const menuElement = document.getElementById("menu");
+const gameContainerElement = document.getElementById("game-container");
+const startButton = document.getElementById("start-button");
+const choices = document.getElementById("choices");
+const resultElement = document.getElementById("result");
+const playerScoreElement = document.getElementById("player-score");
+const computerScoreElement = document.getElementById("computer-score");
+const drawCountElement = document.getElementById("draw-count");
+const instructionsButton = document.getElementById("instructions-button");
+const modalElement = document.getElementById("modal");
+const closeModalButton = document.getElementById("close-modal-button");
+const winnerModalElement = document.getElementById("winner-modal");
+const closeWinnerModalButton = document.getElementById("close-winner-modal");
 
-document.getElementById("start-button").addEventListener("click", function() {
-    // Show the game container and hide the menu
-    document.getElementById("menu").style.display = "none";
-    document.getElementById("game-container").style.display = "block";
-});
-
-document.getElementById("game-title").addEventListener("click", function() {
-    document.getElementById("game-container").style.display = "none";
-    document.getElementById("menu").style.display = "block";
-});
-
-document.getElementById("instructions-button").addEventListener("click", function() {
-    const instructions = "How to Play:\n\n"
-        + "1. Pick between rock, paper, or scissors.\n"
-        + "2. The computer will also choose from one of the options.\n"
-        + "3. Determine the winner based on the rules:\n"
-        + "   - Rock beats scissors.\n"
-        + "   - Paper beats rock.\n"
-        + "   - Scissors beat paper.\n"
-        + "4. Keep track of your wins and losses.";
-    
-    alert(instructions);
-});
-
+// Initialize scores
 let playerScore = 0;
 let computerScore = 0;
 let drawCount = 0;
 
-document.getElementById("start-button").addEventListener("click", function() {
-    document.getElementById("menu").style.display = "none";
-    document.getElementById("game-container").style.display = "block";
+const WINNING_SCORE = 10;
+
+// Event listener for "Start" button and "Game Title" click
+startButton.addEventListener("click", toggleGameContainer);
+document.getElementById("game-title").addEventListener("click", toggleGameContainer);
+
+// Event listener for game title (logo)
+document.getElementById("game-title").addEventListener("click", function() {
+    // Hide the game container and show the menu
+    gameContainerElement.style.display = "none";
+    menuElement.style.display = "block";
 });
 
-const choices = document.querySelectorAll(".choice-btn");
-choices.forEach(choice => {
-    choice.addEventListener("click", function() {
-        const playerSelection = this.id;
-        const computerSelection = createComputerChoice();
-        const result = playGame(playerSelection, computerSelection);
-        displayResult(result, playerSelection, computerSelection);
-        updateScore(result);
-    });
+// Event listener for "Instructions" button
+instructionsButton.addEventListener("click", function() {
+    showModal("modal");
 });
 
+// Event delegation for choice buttons
+choices.addEventListener("click", function(event) {
+    const playerSelection = event.target.id;
+    const computerSelection = createComputerChoice();
+    const result = playGame(playerSelection, computerSelection);
+    displayResult(result, playerSelection, computerSelection);
+    updateScore(result);
+    checkWinner(); // Check if a player has won after each round
+});
 
-// Creates a random choice for computer
+// Function to toggle game container and menu display
+function toggleGameContainer() {
+    menuElement.style.display = menuElement.style.display === "block" ? "none" : "block";
+    gameContainerElement.style.display = gameContainerElement.style.display === "none" ? "block" : "none";
+}
+
+// Function to create a random choice for the computer
 function createComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
 
-// Runs a single round of the game and gives the result
+// Function to run a single round of the game and gives the result
 function playGame(player, computer) {
     if (player === computer) {
         drawCount++;
         return "It's a tie!";
-    } else if ((player === "rock" && computer === "scissors") || 
-               (player === "paper" && computer === "rock") ||
-               (player === "scissors" && computer === "paper")) {
+    } else if ((player === "rock" && computer === "scissors") ||
+        (player === "paper" && computer === "rock") ||
+        (player === "scissors" && computer === "paper")) {
         playerScore++;
         return "You win!";
     } else {
@@ -68,24 +75,57 @@ function playGame(player, computer) {
     }
 }
 
-
-// Displays the result of the game round
+// Function to display the result of the game round
 function displayResult(result, player, computer) {
-    const resultElement = document.getElementById("result");
     resultElement.style.display = "block";
     resultElement.textContent = `${result} You chose ${player}. Computer chose ${computer}.`;
 }
 
-
-// Updates the score and draw tally
+// Function to update the score and draw tally
 function updateScore(result) {
-    const playerScoreElement = document.getElementById("player-score");
-    const computerScoreElement = document.getElementById("computer-score");
-    const drawCountElement = document.getElementById("draw-count");
     playerScoreElement.textContent = `Player: ${playerScore}`;
     computerScoreElement.textContent = `Computer: ${computerScore}`;
     drawCountElement.textContent = `Draws: ${drawCount}`;
 }
 
+// Function to show the modal
+function showModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.style.display = "block";
+}
 
+// Event listener for "Close" button in the modal
+closeModalButton.addEventListener("click", function() {
+    modalElement.style.display = "none";
+});
 
+// Event listener for "Close" button in the winner modal
+closeWinnerModalButton.addEventListener("click", function() {
+    winnerModalElement.style.display = "none";
+});
+
+// Function to reset scores
+function resetScores() {
+    playerScore = 0;
+    computerScore = 0;
+    drawCount = 0;
+    updateScore(); // Update the displayed scores after resetting
+}
+
+// Function to check if a player has won the game
+function checkWinner() {
+    if (playerScore === WINNING_SCORE) {
+        displayWinnerMessage("Congratulations! You won the game!");
+        resetScores();
+    } else if (computerScore === WINNING_SCORE) {
+        displayWinnerMessage("Game Over! The computer won the game!");
+        resetScores();
+    }
+}
+
+// Function to display the winner message in the modal
+function displayWinnerMessage(message) {
+    const winnerMessageElement = document.getElementById("winner-message");
+    winnerMessageElement.textContent = message;
+    showModal("winner-modal");
+}
